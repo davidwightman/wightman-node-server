8b927ba88ca32169eb78cda9d95054793d3b1ee7
David Wightman <david@davidwightman.org>
2019-10-27 15:00:49 -0400

    finished authentication

--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,63 @@
 
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+// const passport = require('passport')
+// const GoogleStrategy = require('passport-google-oauth20').Strategy
+// const mongoose = require('mongoose');
+
+// const keys = require('../config/keys');
+
+// const User = mongoose.model('users');
+
+// passport.use(new GoogleStrategy({
+//     clientID: keys.googleClientID,
+//     clientSecret: keys.googleClientSecret,
+//     callbackURL: '/auth/google/callback'
+// }, (accessToken, refreshToken, profile, done) => {
+//     new User({ googleId: profile.id}).save()
+// }));
+
+
+
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken)
-    console.log('refresh token', refreshToken)
-    console.log('profile', profile)
-}));
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then((user) => {
+        done(null, user);
+    }).catch(e => console.log('Error', e))
+});
+
+passport.use(new googleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL:'/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })
+            .then((existingUser)=>{
+                if (existingUser) {
+                      //user exists
+                     
+                      done(null, existingUser)
+                }else{
+                    //user not exist
+                   
+                    new User({ googleId :profile.id })
+                      .save()
+                      .then(user => done(null, user))
+                      .catch(e => console.log('Error', e))
+                }
+            }).catch(e => console.log('Error', e))
+        
+        }
+    
+));
