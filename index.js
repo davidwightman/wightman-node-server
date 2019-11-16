61d3bce5ee52ddbb4074e152c99449dc63a33f44
David Wightman <david@davidwightman.org>
2019-11-16 16:58:03 -0500

    survey

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User')
+require('./models/Survey')
+
 require('./services/passport')
 
 mongoose.connect(keys.MongoURI, {
@@ -28,6 +30,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     // express will serve production assets for main.js and main.css
