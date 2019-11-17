c5c96b4255cfc94acc55a13ca6ffbc702e94f146
David Wightman <david@davidwightman.org>
2019-11-17 17:47:42 -0500

    finished survey template

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients}, content) {
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@emaily.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -13,7 +14,6 @@ class Mailer extends helper.Mail {
 
         this.addContent(this.body);
         this.addClickTracking();
-
         this.addRecipients();
     }
 
@@ -24,11 +24,29 @@ class Mailer extends helper.Mail {
     }
 
     addClickTracking() {
-        const trackingSetting = new helper.TrackingSettings();
+        const trackingSettings = new helper.TrackingSettings();
         const clickTracking = new helper.ClickTracking(true, true);
-
         trackingSettings.setClickTracking(clickTracking);
         this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = await this.sgApi.API(request);
+        return response;
     }
 }
 
